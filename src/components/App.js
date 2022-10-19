017df4a8be1cf40bcb3d20a27b8d3e3c8a165bf2
Seth Feldman <namdlefhtes@gmail.com>
2022-10-18 21:03:36 -0400

    basic navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import '../App.css';
 // import { Container } from'@mui/material';
 import SignUp from "./SignUp";
+import NavBar from "./NavBar";
 import { AuthContextProvider } from "../contexts/AuthContext";
 
 function App() {
   return (
     <AuthContextProvider>
       <div className="App">
+        <NavBar/>
         <h1>Hockey Stat Tracker</h1>
         <SignUp />
       </div>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+
+export default function NavBar() {
+    return(
+    <AppBar position="static">
+        <Toolbar>
+            <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}>
+        </IconButton>
+        <Avatar/>
+        <h3>Name</h3>
+        </Toolbar>
+    </AppBar>
+    )
+}

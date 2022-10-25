81604405456dbd7cabb208098b05bafacc80beba
Seth Feldman <namdlefhtes@gmail.com>
2022-10-25 14:46:33 -0400

    clean up

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,17 +2,6 @@ import React from 'react';
 import StatEntry from "./StatEntry";
 import PointHistory from "./PointHistory";
 
-import { collection, addDoc } from "firebase/firestore"; 
-
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-
 import {UserAuth} from '../contexts/AuthContext';
 
 export default function Dashboard(props) {
@@ -21,11 +10,17 @@ export default function Dashboard(props) {
 
     const {user} = UserAuth();
 
+    const [detectChange , setDetectChange] = React.useState();
+
+    const causeRender = () => {
+        setDetectChange(true);
+    };
+
     return (
         <>
             <div>Welcome, {props.activeUser.name}</div>
             <StatEntry activeUser = {props.activeUser} />
-            <PointHistory activeUser = {props.activeUser} />
+            <PointHistory activeUser = {props.activeUser} detectChange = {detectChange} />
         </>
     )
 }

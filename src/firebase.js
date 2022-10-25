d77c0b4a0b168aa40d14c7a2fb0f13d0a80270e4
Seth Feldman <namdlefhtes@gmail.com>
2022-10-25 14:47:52 -0400

    clean up

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from "firebase/analytics";
-import firebase from "firebase/app";
 import "firebase/auth";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";

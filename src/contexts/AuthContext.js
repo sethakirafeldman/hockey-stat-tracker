df7c851330bfd3b4bfb73c6da9504a0e95bcb919
Seth Feldman <namdlefhtes@gmail.com>
2022-10-27 18:14:14 -0400

    redirect causes error, changed back to pop up

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -18,7 +18,7 @@ export const AuthContextProvider = ( {children} ) => {
 
     const googleSignIn = () => {
         const provider = new GoogleAuthProvider();
-        signInWithRedirect(auth, provider);
+        signInWithPopup(auth, provider);
     };
 
     const logOut = () => {

789de7d9f4b6b4fc586b8b626e178811b2df0d3b
Seth Feldman <namdlefhtes@gmail.com>
2022-10-27 11:30:56 -0400

    switched auth to redirect

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -18,7 +18,7 @@ export const AuthContextProvider = ( {children} ) => {
 
     const googleSignIn = () => {
         const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider);
+        signInWithRedirect(auth, provider);
     };
 
     const logOut = () => {

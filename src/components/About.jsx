81ec3ee97bb256070aea83c483f3361f2cca026d
namdlefhtes <namdlefhtes@gmail.com>
2022-10-31 20:53:01 -0400

    about comp

---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <section>
+            <h3>About</h3>
+            <p>This site was built by Seth Feldman. 
+                It was built as a tool for me (Seth) and 
+                his friends to use in order to track
+                stats for their adult hockey league.  
+
+                This was built using a number of libraries 
+                including MUI, React, React Router, and it uses Firebase 
+                for its backend. 
+            </p>
+        </section>
+    )
+}

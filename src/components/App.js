fe78bd17e537a318ddc1546bdd59813ca8214f89
Seth Feldman <namdlefhtes@gmail.com>
2022-11-03 12:34:55 -0400

    moved current date func to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,17 @@ import Sharpens from "./Sharpens";
 import About from "./About";
 import Graphs from "./Graphs";
 
+//react
 import React, { useState, useEffect  } from 'react';
 import {BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+// day js
+import dayjs from 'dayjs'
+
+//mui
 import Box from '@mui/material/Box';
 
+//firestore / auth
 import {UserAuth} from '../contexts/AuthContext';
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
@@ -19,9 +25,28 @@ function App() {
   const {user} = UserAuth();
   
   const [activeUser, setActiveUser] = useState({});
+  const [currentDate, setCurrentDate] = useState();
+  
+  // set date and timezone
+  const dateToday = () => {
+    let year = dayjs().year();
+    let month = dayjs().month() + 1;
+    let day = dayjs().date();
+    
+    if (month < 10 && day < 10 ) {
+      month = `0${month}`;
+      day = `0${day}`;
+    }
+    else if (day < 10) {
+      day = `0${day}`;
+    }
+    else if (month < 10) {
+      month = `0${month}`;
+    }
+    setCurrentDate(`${year}-${month}-${day}`);
+  };  
 
   // retrieve player data from firestore
- 
   useEffect (() => {
     let playerData = {};
     async function getPlayer() {
@@ -47,7 +72,7 @@ function App() {
         console.log(err)
       }
     };
-
+    dateToday();
     getPlayer();
   
   }, [user]);
@@ -57,7 +82,7 @@ function App() {
       
       <Box>
       <BrowserRouter>
-        {Object.keys(activeUser).length == 0 ? 
+        {Object.keys(activeUser).length === 0 ? 
         <>
         <NavBar/>
         <SignUp />
@@ -74,8 +99,8 @@ function App() {
           :
           <>
             <Route path="/" element={<Navigate replace to="/dashboard" />} />
-            <Route path = "/dashboard" element = {<Dashboard activeUser = {activeUser} /> } />
-            <Route path = "/sharpens" element = { <Sharpens />} />
+            <Route path = "/dashboard" element = {<Dashboard activeUser = {activeUser} currentDate = {currentDate}/> } />
+            <Route path = "/sharpens" element = { <Sharpens activeUser = {activeUser} currentDate = {currentDate}/>} />
             <Route path = "/about" element = {<About />} />
             <Route path = "/graphs" element = { <Graphs /> } />
           </>

9a9f3fcf2c005c95d13ba3f4ddee3fcf67443a9d
Seth Feldman <namdlefhtes@gmail.com>
2022-11-22 21:43:24 -0500

    build fix

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -3,16 +3,16 @@ import { Card, Typography, CardContent, Box } from '@mui/material';
 
 import {UserAuth} from '../contexts/AuthContext';
 
-export default function ({activeUser}) {
+export default function Settings ({activeUser}) {
     const {user} = UserAuth();
-    
+
     return (
         <>        
         <Typography variant="h4" gutterBottom sx = {{mt: 2}}>Settings</Typography>
         <Box display ="flex"  alignItems="center" justifyContent="center" sx = {{minWidth: 275}}>
         <Card variant = "outlined" sx={{ minWidth: 275, textAlign: "center" }}>
         <CardContent>
-        <img src = {user.photoURL}></img>
+        <img alt = {user.displayName} src = {user.photoURL}></img>
         <Typography sx={{ fontSize: 16 }} color="text.primary" gutterBottom>
             {user.displayName}
         </Typography>

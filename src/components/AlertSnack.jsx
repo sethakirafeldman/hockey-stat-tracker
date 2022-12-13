6fc2c7899cf1fc49d1ae875529bda87f39fdadfb
Seth Feldman <namdlefhtes@gmail.com>
2022-12-12 22:48:22 -0500

    build clean up

--- src/components/AlertSnack.jsx
+++ src/components/AlertSnack.jsx
@@ -1,23 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
-import Button from '@mui/material/Button';
 
 export default function AlertSnack({openSnack, onClose, type, text}) {
-
-    const [open, setOpen] = useState(false);
-
+  
     const Alert = React.forwardRef(function Alert(props, ref) {
         return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
       });
-
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-          return;
-        }
-    
-        setOpen(false);
-    };  
 
     return (
     <>
